dcab42e2ea43d29f0ec4603e854ed0aa47a144aa
Jens Kalshoven <jens.kalshoven@gmail.com>
2024-12-06 21:55:16 +0100

    Day 6 - Fast (500ms)

--- src/days/day6/__init__.py
+++ src/days/day6/__init__.py
@@ -78,43 +78,44 @@ class AdventDay(AbstractAdventDay):
         [height, width] = data.grid.shape
         object_positions = [(int(coord[0]), int(coord[1])) for coord in np.argwhere(data.grid == '#')]
 
+        # Make map of movements to the next object for each incoming direction to an object
         movement_map = dict()
         for (r, c) in object_positions:
-            movement_map[(r, c)] = {
-                'N': find_next_obstacle((r + 1, c), 'N', data),
-                'E': find_next_obstacle((r, c - 1), 'E', data),
-                'S': find_next_obstacle((r - 1, c), 'S', data),
-                'W': find_next_obstacle((r, c + 1), 'W', data),
-            }
+            movement_map[(r, c)] = generate_movement_map(data, r, c)
 
-        loops_detected = set()
+        # Generate the path that the guard walks without extra object
         default_guard = Guard(data.start_pos)
         generate_guard_path(default_guard, height, width, object_positions)
-        for (obstacle_row, obstacle_col, orientation) in default_guard.steps[1:]:
-            if (obstacle_row, obstacle_col) in loops_detected:
+
+        loop_detected = set()
+        no_loop_detected = set()
+
+        for (obstacle_row, obstacle_col, _) in default_guard.steps[1:]:
+            if (obstacle_row, obstacle_col) in loop_detected or (obstacle_row, obstacle_col) in no_loop_detected:
                 continue
 
-            obstacle_movement = {
-                'N': find_next_obstacle((obstacle_row + 1, obstacle_col), 'N', data),
-                'E': find_next_obstacle((obstacle_row, obstacle_col - 1), 'E', data),
-                'S': find_next_obstacle((obstacle_row - 1, obstacle_col), 'S', data),
-                'W': find_next_obstacle((obstacle_row, obstacle_col + 1), 'W', data),
-            }
+            obstacle_movement = generate_movement_map(data, obstacle_row, obstacle_col)
 
             current, direction = find_next_obstacle(data.start_pos, 'W', data, (obstacle_row, obstacle_col))
+
+            # Handle if the path intersects with the extra obstacle
             if current == (obstacle_row, obstacle_col):
                 result = obstacle_movement[direction]
 
+                # Result is None if there are no more obstacles in the way
                 if result is None:
                     continue
 
                 (current, direction) = result
 
-            visited = {(*current, direction)}
+            visited = {(current, direction)}
+
             while True:
                 result = movement_map[current][direction]
 
+                # Result is None if there are no more obstacles in the way
                 if result is None:
+                    # Handle if the path intersects with the extra obstacle
                     if direction == 'N' and current[0] + 1 == obstacle_row and current[1] < obstacle_col:
                         result = obstacle_movement['E']
                     elif direction == 'E' and current[1] - 1 == obstacle_col and current[0] < obstacle_row:
@@ -125,7 +126,8 @@ class AdventDay(AbstractAdventDay):
                         result = obstacle_movement['N']
                 else:
                     (next_pos, next_dir) = result
-                    # Detect if intersects with extra obstacle
+
+                    # Handle if the path intersects with the extra obstacle
                     if (
                             (next_dir == 'N' and next_pos[1] == obstacle_col and current[0] > obstacle_row > next_pos[0]) or
                             (next_dir == 'E' and next_pos[0] == obstacle_row and current[1] < obstacle_col < next_pos[1]) or
@@ -135,65 +137,30 @@ class AdventDay(AbstractAdventDay):
                         result = obstacle_movement[next_dir]
 
                 if result is None:
+                    no_loop_detected.add((obstacle_row, obstacle_col))
                     break
 
                 (current, direction) = result
-                if (*current, direction) in visited:
-                    loops_detected.add((obstacle_row, obstacle_col))
+                if (current, direction) in visited:
+                    loop_detected.add((obstacle_row, obstacle_col))
                     break
 
-                visited.add((*current, direction))
+                visited.add((current, direction))
 
-        return len(loops_detected)
+        return len(loop_detected)
 
-    @expected_answers(example_answer=6, answer=1915)
-    def puzzle_2_slow(self, data: Data) -> int:
-        [height, width] = data.grid.shape
-        object_positions = [(coord[0], coord[1]) for coord in np.argwhere(data.grid == '#')]
-
-        default_guard = Guard(data.start_pos)
-        generate_guard_path(default_guard, height, width, object_positions)
-
-        not_looping_options = set()
-        looping_options = set()
-        for i, (row, col, orientation) in enumerate(default_guard.steps, start=1):
-            print(f"{i}/{len(default_guard.steps)}")
-
-            forward_pos = {
-                'N': lambda r, c: (r - 1, c),
-                'E': lambda r, c: (r, c + 1),
-                'S': lambda r, c: (r + 1, c),
-                'W': lambda r, c: (r, c - 1),
-            }[orientation](row, col)
-
-            if forward_pos in looping_options or forward_pos in not_looping_options:
-                continue
-
-            guard = Guard(data.start_pos)
-            if not generate_guard_path(guard, height, width, [*object_positions, forward_pos]):
-                looping_options.add(forward_pos)
-            else:
-                not_looping_options.add(forward_pos)
-
-        return len(looping_options)
 
 def generate_guard_path(
         guard: Guard,
         height: int,
         width: int,
         object_positions: list[tuple[int, int]],
-) -> bool:
+):
     while 0 <= guard.pos[0] < height and 0 <= guard.pos[1] < width:
         while guard.forward_pos() in object_positions:
             guard.turn_right()
 
-        # Check for loop
-        if (*guard.pos, guard.orientation) in guard.steps:
-            return False
-
         guard.move_forward()
-
-    return True
 
 
 def find_next_obstacle(start: tuple[int, int], direction: str, data: Data, extra_obstacle: tuple[int, int] = None) -> tuple[tuple[int, int], str] | None:
@@ -214,3 +181,11 @@ def find_next_obstacle(start: tuple[int, int], direction: str, data: Data, extra
             return next_pos, direction
 
         current = next_pos
+
+def generate_movement_map(data: Data, obstacle_row: int, obstacle_col: int):
+    return {
+        'N': find_next_obstacle((obstacle_row + 1, obstacle_col), 'N', data),
+        'E': find_next_obstacle((obstacle_row, obstacle_col - 1), 'E', data),
+        'S': find_next_obstacle((obstacle_row - 1, obstacle_col), 'S', data),
+        'W': find_next_obstacle((obstacle_row, obstacle_col + 1), 'W', data),
+    }
